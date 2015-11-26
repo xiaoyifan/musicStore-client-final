9b4e86e225d51e7f19695d9894ae9e16f556fe34
xiaoyifan <xiaoyifan@uchicago.edu>
2015-11-26 14:53:02 -0600

    solve insert duplicate problem

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,13 +4,15 @@ class ProductsController < ApplicationController
 
    def index
   #@products = Product.all
-     json = open("http://localhost:4000/inventory.json").read
+    json = open("http://localhost:4000/inventory.json").read
     puts "###################"
     puts json
     puts "###################"
     @products = JSON.parse(json)
     @products.each do |product|
-      Product.create sku: product["sku"], photo_url: product["photo_url"], title: product["title"], description: product["description"], price: product["price"]
+
+      item = Product.find_by(sku: product["sku"])
+      item ||= Product.create( sku: product["sku"], photo_url: product["photo_url"], title: product["title"], description: product["description"], price: product["price"])
       # Product.create sku: 'B0002F58TG', photo_url: 'http://ecx.images-amazon.com/images/I/61fspjyu4yL._SL1500_.jpg', title: 'Jasmine S35 Acoustic Guitar', description: 'This Amazon Marketplace top-selling acoustic guitar is the perfect instrument for a beginner of any age, or as a second instrument for the seasoned player. From its detailed appointments to the bold acoustic tones it projects, the Jasmine S35 dreadnought guitar is an excellent all around solution for any style of music. This finely crafted instrument is backed by a limited lifetime warranty.'
     end
    end
